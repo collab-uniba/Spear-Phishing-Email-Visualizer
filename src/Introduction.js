98717c87712b490d678fa960c3e1b5bb20ab8c2c
Multipask <pasquale.monniello00@gmail.com>
2022-08-12 18:20:31 +0200

    App complete. Need to refresh Emails

--- src/Introduction.js
+++ src/Introduction.js
@@ -3,7 +3,9 @@ import React from 'react'
 const Introduction = (props) => {
   return (
     <div className="Intro">
-        Benvenuto/a nel test
+        <div>Benvenuto/a nel test. Giudica le email qui sotto</div>
+        <div>Premi <strong>"Phishing"</strong> se pensi che la mail sia phishing</div>
+        <div>Premi <strong>"Legittima"</strong> se pensi che la mail sia legittima</div>
     </div>
   )
 }
