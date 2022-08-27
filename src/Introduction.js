0fada90786417bee94329deee5c96bbc945aeac5
Multipask <pasquale.monniello00@gmail.com>
2022-08-27 10:49:28 +0200

    Introduction changed.

--- src/Introduction.js
+++ src/Introduction.js
@@ -4,6 +4,8 @@ const Introduction = (props) => {
   return (
     <div className="Intro">
         <div>Benvenuto/a nel test. Giudica le email qui sotto</div>
+        <div><strong>ATTENZIONE</strong> se clicchi su uno dei link nelle email, vuol dire che</div>
+        <div>ti stai fidando della mail. Quindi la scelta che devi prendere è <strong>legittima</strong></div>
         <div>Premi <strong>"Phishing"</strong> se pensi che la mail sia phishing</div>
         <div>Premi <strong>"Legittima"</strong> se pensi che la mail sia legittima</div>
     </div>
